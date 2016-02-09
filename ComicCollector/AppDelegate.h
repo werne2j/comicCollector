d20103cd4c25579243487f61b3b86e297b258c3c
JACOB A. WERNETTE <jawernet@us.ibm.com>
2016-02-08 21:01:23 -0500

    initial commit

---
+++ ComicCollector/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ComicCollector
+//
+//  Created by Jacob Wernette on 2/8/16.
+//  Copyright © 2016 Jacob Wernette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ ComicCollector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ComicCollector
+//
+//  Created by Jacob Wernette on 2/8/16.
+//  Copyright © 2016 Jacob Wernette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
